7ed121f63b70c8a5acd1c9a909788be1c4c09f7d
yang991 <y@github.com>
2025-06-10 18:42:24 -0700

    make event loop thread daemonic so that it's destroyed when main thread exits

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
         print("\033[33m{}\033[0m".format("注意：python 版本号不正确，可能无法正常运行"))
         print("建议python版本：3.10.9 当前：" + sys.version)
     register_app(UmamusumeManifest)
-    scheduler_thread = threading.Thread(target=scheduler.init, args=())
+    scheduler_thread = threading.Thread(target=scheduler.init, args=(), daemon=True)
     scheduler_thread.start()
     print("UAT running on http://127.0.0.1:8071")
     run("bot.server.handler:server", host="127.0.0.1", port=8071, log_level="error")
